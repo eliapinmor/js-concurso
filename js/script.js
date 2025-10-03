76a2f5b2e9cb352e217774a9ba9184a4167d548c
elia <eliapinmor@monlau.com>
2025-10-03 18:14:34 +0200

    preguntas reordenadas, respuestas han de ser reordenadas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let isLogin = false;
-let
+let contador = 0;
 
 function cargarPreguntas() {
+    const preguntasDiv = document.querySelector(".preguntas");
+    preguntasDiv.style.display = "block";
     const container = document.querySelector(".row");
     container.innerHTML = "";
     let indice = 0;
@@ -11,9 +13,12 @@ function cargarPreguntas() {
     fetch('../json/data.json')
         .then(response => response.json())
         .then(data => {
-            const preguntasMostradas = data.preguntas.slice(indice, indice + 2);
+            const dataNewSort = data.preguntas.sort(() => Math.random() - 0.5);
+            const preguntasMostradas = dataNewSort.slice(indice, indice + 2);
 
             preguntasMostradas.forEach(pregunta => {
+                const opcionesNewSort = pregunta.opciones.sort(() => Math.random() - 0.5);
+
                 container.innerHTML += `
                             <div class="col-lg-6 col-md-6 col-12 mb-4 card-item">
                                 <div class="card h-100">
@@ -21,24 +26,23 @@ function cargarPreguntas() {
                                     <div class="card-body">
                                         <div style="display: flex; flex-direction: column; gap: 6px;">
                                             <label>
-                                                <input type="radio" name="${pregunta.id}" value="opcion-1">${pregunta["opcion1"]}
+                                                <input type="radio" name="${pregunta.id}" value="opcion-1">${opcionesNewSort[0]}
                                             </label>
                                             <label>
-                                                <input type="radio" name="${pregunta.id}" value="opcion-2">${pregunta["opcion2"]}
+                                                <input type="radio" name="${pregunta.id}" value="opcion-2">${opcionesNewSort[1]}
                                             </label>
                                             <label>
-                                                <input type="radio" name="${pregunta.id}" value="opcion-3">${pregunta["opcion3"]}
+                                                <input type="radio" name="${pregunta.id}" value="opcion-3">${opcionesNewSort[2]}
                                             </label>
                                             <label>
-                                                <input type="radio" name="${pregunta.id}" value="opcion-4">${pregunta["opcion4"]}
+                                                <input type="radio" name="${pregunta.id}" value="opcion-4">${opcionesNewSort[3]}
                                             </label>
                                         </div>
-
                                     </div>
                                 </div>
                             </div>
                     `;
-                
+
 
             });
         })
@@ -50,17 +54,29 @@ function iniciarPartida() {
     const username = document.querySelector(".username").value;
     const texto = document.querySelector(".saludo");
 
-    texto.innerHTML = "Buenas, " + username;
-    isLogin = true;
-    login.style.display = "none";
-    cargarPreguntas();
+    // if (username != "") {
+        texto.innerHTML = "Buenas, " + username;
+        isLogin = true;
+        login.style.display = "none";
+        cargarPreguntas();
+    // } else {
+    //     alert("Introduzca un nombre");
+
+    // }
+
+
 }
 
 
 function checkAnswers() {
-    let respuesta = document.querySelector('input[name="${pregunta.id}"]:checked');
+
+    dataNewSort[contador]
+
+
+
+
+
+
+    // let respuesta = document.querySelector('input[name="${pregunta.id}"]:checked');
 }
 
-window.onload = function () {
-    cargarPreguntas();
-}
