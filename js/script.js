8c95ae25742606cab1060054ac8e47683963fbd9
eliapinmor <eliapinmor@monlau.com>
2025-10-07 14:57:43 +0200

    mensaje de error y css nuevo, se ha empezadoa  crear la estructura para guardar los datos

--- js/script.js
+++ js/script.js
@@ -3,43 +3,55 @@ let contador = 0;
 let indice = 0;
 let dataNewSort = [];
 let i_name = 0;
-let isCorrect = false;
+let isGanado = false;
 let preguntasMostradas = [];
+let storage = [];
+let usernameObj = "";
 
 
 function cargarPreguntas() {
-    const preguntasDiv = document.querySelector(".preguntas");
-    preguntasDiv.style.display = "block";
-    const container = document.querySelector(".row");
-    container.innerHTML = "";
-    const boton_enviar = document.querySelector(".boton_enviar");
-    boton_enviar.style.display = "block";
+  const preguntasDiv = document.querySelector(".preguntas");
+  preguntasDiv.style.display = "block";
+  const container = document.querySelector(".row");
+  container.innerHTML = "";
+  const boton_enviar = document.querySelector(".boton_enviar");
+  boton_enviar.style.display = "block";
 
-    fetch('../json/data.json')
-        .then(response => response.json())
-        .then(data => {
-            if (indice === 0) {
-                dataNewSort = data.preguntas.sort(() => Math.random() - 0.5);
-            }
-            preguntasMostradas = dataNewSort.slice(indice, indice + 2);
+  fetch('../json/data.json')
+    .then(response => response.json())
+    .then(data => {
+      if (indice === 0) {
+        dataNewSort = data.preguntas.sort(() => Math.random() - 0.5);
+      }
+      preguntasMostradas = dataNewSort.slice(indice, indice + 2);
+      console.log(preguntasMostradas[0]);
+      console.log(preguntasMostradas[1]);
+      console.log(preguntasMostradas[2]);
+      console.log(preguntasMostradas[3]);
+      console.log(preguntasMostradas[4]);
+      console.log(preguntasMostradas[5]);
+      console.log(preguntasMostradas[6]);
+      console.log(preguntasMostradas[7]);
+      console.log(preguntasMostradas[8]);
+      console.log(preguntasMostradas[9]);
 
-            preguntasMostradas.forEach(pregunta => {
-                const opcionesArray = pregunta.opciones.map(o => {
-                    console.log(Object.keys(o))
-                    const key = Object.keys(o)[0];
-                    return { key, text: o[key] };
-                });
+      preguntasMostradas.forEach(pregunta => {
+        const opcionesArray = pregunta.opciones.map(o => {
+          console.log(Object.keys(o))
+          const key = Object.keys(o)[0];
+          return { key, text: o[key] };
+        });
 
-                const opcionesNewSort = opcionesArray.sort(() => Math.random() - 0.5);
+        const opcionesNewSort = opcionesArray.sort(() => Math.random() - 0.5);
 
-                const opcionesHTML = opcionesNewSort.map((op, i) => `
+        const opcionesHTML = opcionesNewSort.map((op, i) => `
                     <label>
                         <input type="radio" name="${i_name}" value="${op.key}">
                         ${op.text}
                     </label>
                     `).join("");
 
-                container.innerHTML += `
+        container.innerHTML += `
           <div class="col-lg-6 col-md-6 col-12 mb-4 card-item">
             <div class="card h-100">
               <p>${pregunta.pregunta}</p>
@@ -51,46 +63,122 @@ function cargarPreguntas() {
             </div>
           </div>
         `;
-                i_name++;
-    });
-        })
-        .catch(error => console.error('Error al cargar JSON:', error));
+        i_name++;
+      });
+    })
+    .catch(error => console.error('Error al cargar JSON:', error));
 }
 
 function iniciarPartida() {
-    const login = document.querySelector(".login");
-    const username = document.querySelector(".username").value;
-    const texto = document.querySelector(".saludo");
+  const login = document.querySelector(".login");
+  const username = document.querySelector(".username").value;
+  const texto = document.querySelector(".saludo");
+  const mensaje = document.querySelector(".mensaje");
+  mensaje.innerHTML = "";
+  usernameObj = username;
 
-    // if (username != "") {
+  if (username != "") {
     texto.innerHTML = "Buenas, " + username;
     isLogin = true;
     login.style.display = "none";
     cargarPreguntas();
-    // } else {
-    //     alert("Introduzca un nombre");
-
-    // }
+  } else {
+    alert("Introduzca un nombre")
+  }
 
 
 }
 
 
 function checkAnswers() {
-    const respuesta1 = document.querySelector(`input[name="${contador}"]:checked`).value;
-    console.log(respuesta1);
-        if (respuesta1 === preguntasMostradas[contador].opcion_correcta){
-        console.log("opcion correcta");
-    }
-    contador++;
+  const mensaje = document.querySelector(".mensaje");
+  mensaje.innerHTML = "";
+  const respuesta1 = document.querySelector(`input[name="${contador}"]:checked`).value;
+  console.log(contador);
 
-    const respuesta2 = document.querySelector(`input[name="${contador}"]:checked`).value;
+  if (respuesta1 !== preguntasMostradas[0].opcion_correcta) {
 
-    console.log(respuesta2);
+    mensaje.innerHTML = "Respuesta incorrecta, volviendo a inicio...";
+    guardarDatos();
+    setTimeout(() => {
+      volverInicio();
+    }, 1000);
+    return;
+  }
+  contador++;
+  const respuesta2 = document.querySelector(`input[name="${contador}"]:checked`).value;
+  console.log(contador);
 
+  if (respuesta2 !== preguntasMostradas[1].opcion_correcta) {
+    mensaje.innerHTML = "Respuesta incorrecta, volviendo a inicio...";
+    guardarDatos();
+    setTimeout(() => {
+      volverInicio();
+    }, 1000);
+    return;
+  }
+  console.log(respuesta2);
+  contador++;
 
-    indice += 2;
-    cargarPreguntas();
+  if (contador >= dataNewSort.length - 1) {
+    mensaje.style.color = "green";
+    mensaje.innerHTML = "Has ganado";
+    isGanado = true;
+    guardarDatos();
+    setTimeout(() => {
+      volverInicio();
+    }, 1000);
+    return;
+  }
+
+  indice += 2;
+  cargarPreguntas();
 
 }
 
+
+function volverInicio() {
+  const login = document.querySelector(".login");
+  login.style.display = "block";
+
+  const preguntasDiv = document.querySelector(".preguntas");
+  preguntasDiv.style.display = "none";
+
+  const container = document.querySelector(".row");
+  container.style.display = "";
+  container.innerHTML = "";
+
+
+  const saludo = document.querySelector(".saludo");
+  saludo.style.display = "none";
+
+  const boton_enviar = document.querySelector(".boton_enviar");
+  boton_enviar.style.display = "none";
+
+  const mensaje = document.querySelector(".mensaje");
+  mensaje.innerHTML = "";
+  mensaje.style.color = "";
+
+
+
+  isLogin = false;
+  contador = 0;
+  indice = 0;
+  dataNewSort = [];
+  i_name = 0;
+  preguntasMostradas = [];
+  isGanado = false;
+  usernameObj = "";
+
+}
+
+function guardarDatos(){
+  //crear un objeto y guardarlo en el array storage[]
+  let newObject = {
+    jugador: usernameObj,
+    aciertos: contador
+  };
+  console.log(username);
+  storage.push(newObject)
+
+}
